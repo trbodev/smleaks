33b58227df78d44e43d6be31241787f74aed1b67
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2021-06-01 18:10:55 -0400

    Possibly fix footer text overlap on small width devices

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,28 +5,30 @@ import {
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
 import Link from 'next/link';
+import { isMobile } from 'react-device-detect';
 
 export default function Home() {
   return (
     <nav className="navbar navbar-fixed-bottom">
-      <ul className="navbar-nav ml-auto">
+      <ul className={`navbar-nav ${isMobile ? '' : 'ml-auto'}`}>
         <li className="nav-item">
           <Link href="/discord">
             <a className="nav-link">
-              <FontAwesomeIcon icon={faDiscord} className="mr-5" fixedWidth />
-              Discord
+              <FontAwesomeIcon icon={faDiscord} className={isMobile ? 'mr-2 ml-5' : 'mr-5'} fixedWidth />
+              {isMobile ? '' : 'Discord'}
             </a>
           </Link>
         </li>
         <li className="nav-item">
           <Link href="/twitter">
             <a className="nav-link">
-              <FontAwesomeIcon icon={faTwitter} className="mr-5" fixedWidth />
-              Twitter
+              <FontAwesomeIcon icon={faTwitter} className={isMobile ? 'mr-2' : 'mr-5'} fixedWidth />
+              {isMobile ? '' : 'Twitter'}
             </a>
           </Link>
         </li>
       </ul>
+      <div className={isMobile ? 'ml-auto' : ''} />
       <span className="navbar-text">
         &copy; Copyright
         {' '}
