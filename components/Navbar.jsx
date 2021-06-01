3fa864cfa310fad68e4e8f8286c1b76053342b6d
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2021-06-01 18:25:16 -0400

    Add development tag

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,6 +27,7 @@ export default function Home({
         <a href="/" className="navbar-brand ml-10 ml-sm-20">
           <img src={Logo.toString()} alt="logo" />
           <span className="d-none d-sm-flex">SMLeaks</span>
+          {process.env.NEXT_PUBLIC_DOMAIN_DEV === 'true' ? <span className="badge badge-gray" style={{ marginLeft: 6 }}>Development</span> : <></>}
         </a>
       </Link>
       {process.env.NEXT_PUBLIC_VIEWERS === 'true' ? (
