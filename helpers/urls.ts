58c307c7118cfe5a56aac6af1007681d72bd9d8a
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2021-06-16 16:34:37 -0400

    Fix generateURL for protocols other than http

--- helpers/urls.ts
+++ helpers/urls.ts
@@ -1,7 +1,7 @@
 export function generateURL(
   protocol: string, domain: string, secure: boolean, url: string,
 ) {
-  return `http${secure ? 's' : ''}://${domain}${url}`;
+  return `${protocol}${secure ? 's' : ''}://${domain}${url}`;
 }
 
 export default {
